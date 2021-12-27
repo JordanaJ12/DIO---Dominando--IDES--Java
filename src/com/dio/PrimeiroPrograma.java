68974c93f29e46805df0fd97a6a8e6cdd0dff357
JordanaJ12 <jordana.jesus.2001@gmail.com>
2021-12-27 20:25:07 -0300

    Primeiro Commit Do curso Dominando IDES

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -5,10 +5,7 @@ import com.dio.model.Gato;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		/*int a = 2;
-		int b= 3;
-		System.out.println("Olá Mundo! " + (a+b));
-		*/
+		
 		Gato gato = new Gato();
 		
 		System.out.println(gato);
